7caf431ac1115f1d0bd01a504d35f9363f670a16
crazecoder <21527312@qq.com>
2019-03-04 15:28:03 +0800

    修改iOS为返回InitResultInfo实体

--- ios/Classes/FlutterBuglyPlugin.m
+++ ios/Classes/FlutterBuglyPlugin.m
@@ -4,7 +4,7 @@
 @implementation FlutterBuglyPlugin
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
   FlutterMethodChannel* channel = [FlutterMethodChannel
-      methodChannelWithName:@"flutter_bugly"
+      methodChannelWithName:@"crazecoder/flutter_bugly"
             binaryMessenger:[registrar messenger]];
   FlutterBuglyPlugin* instance = [[FlutterBuglyPlugin alloc] init];
   [registrar addMethodCallDelegate:instance channel:channel];
@@ -12,10 +12,24 @@
 
 - (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
   if ([@"initBugly" isEqualToString:call.method]) {
-    NSString *appId = call.arguments[@"appId"];
-    [Bugly startWithAppId:appId];
-      NSLog(@"Bugly appId: %@", appId);
-    result(@"Bugly 初始化成功");
+      NSString *appId = call.arguments[@"appId"];
+      BOOL b = [self isBlankString:appId];
+      if(!b){
+          [Bugly startWithAppId:appId];
+          NSLog(@"Bugly appId: %@", appId);
+          NSDictionary * dict = @{@"message":@"Bugly 初始化成功", @"isSuccess":@YES};
+          NSData * jsonData = [NSJSONSerialization dataWithJSONObject:dict options:NSJSONWritingPrettyPrinted error:nil];
+          NSString * json = [[NSString alloc] initWithData:jsonData encoding:NSUTF8StringEncoding];
+          
+          result(json);
+      }else{
+          NSDictionary * dict = @{@"message":@"Bugly 初始化失败", @"isSuccess":@NO};
+          NSData * jsonData = [NSJSONSerialization dataWithJSONObject:dict options:NSJSONWritingPrettyPrinted error:nil];
+          NSString * json = [[NSString alloc] initWithData:jsonData encoding:NSUTF8StringEncoding];
+          
+          result(json);
+      }
+      
   }else if([@"postCatchedException" isEqualToString:call.method]){
       NSString *crash_detail = call.arguments[@"crash_detail"];
       NSString *crash_message = call.arguments[@"crash_message"];
@@ -31,8 +45,22 @@
       [Bugly reportException:ex];
       result(nil);
   }else {
-    result(FlutterMethodNotImplemented);
+      result(FlutterMethodNotImplemented);
   }
+}
+- (BOOL) isBlankString:(NSString *)string {
+    if (string == nil || string == NULL) {
+        return YES;
+    }
+    
+    if ([string isKindOfClass:[NSNull class]]) {
+        return YES;
+    }
+    if ([[string stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceCharacterSet]] length]==0) {
+        return YES;
+    }
+    return NO;
+    
 }
 
 @end
